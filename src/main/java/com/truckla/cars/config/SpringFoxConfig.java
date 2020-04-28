c7b2d80af2baef2e466f20694312c111f100d1dc
Valentin Despa <valentin@vdespa.com>
2020-04-28 16:07:27 -0500

    Change API documentation title

--- src/main/java/com/truckla/cars/config/SpringFoxConfig.java
+++ src/main/java/com/truckla/cars/config/SpringFoxConfig.java
@@ -2,8 +2,10 @@ package com.truckla.cars.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,12 +13,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Car fleet management API")
+                .description("The API allows you to manage the car fleet, add repairs and view statistics.")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .build();
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.truckla.cars"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
     }
 }
